c144efa38394f3b6745bced9576ce1ef0b685104
Yuki Nakayama <snow@macaroni.onmicrosoft.com>
2020-10-06 13:02:41 +0900

    Author の一覧をとってきてドロップダウンに表示（フィルター機能は未実装）

---
+++ BlazorApp3/Server/Controllers/AuthorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp3.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp3.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public AuthorsController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var authors = await context.Books.Select(b => b.Author).Distinct().ToListAsync();
+            return Ok(authors);
+        }
+    }
+}
